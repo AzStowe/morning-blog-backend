bcb9aac5f8331088fe400c30e4790c853e77d803
AzStowe <aaron.z.stowe@gmail.com>
2019-07-06 12:30:28 -0500

    fixing proper routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 var express = require('express')
 var router = express.Router()
-
 var postCtrl = require('../controllers/post')
-//edit routes
 
 router.get('/', postCtrl.index)
-router.post('/', postCtrl.create)
 router.get('/:id', postCtrl.show)
-router.delete('/:id/delete', postCtrl.delete)
+router.post('/', postCtrl.create)
+router.delete('/:id/', postCtrl.delete)
 
 module.exports = router
